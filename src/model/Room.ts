331d3f7ed7baa5b87e5b911132a1b0153f897f1a
배윤복 <qodbsqhr@naver.com>
2023-10-08 16:37:41 +0900

    init project

---
+++ src/model/Room.ts
@@ -0,0 +1,9 @@
+class Room {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+export default Room;
---
+++ src/view/Room.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+
+const Room: React.FC = () => (<></>)
+export default Room
